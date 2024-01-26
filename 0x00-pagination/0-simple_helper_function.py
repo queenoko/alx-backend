21c72e047787969b201a6cb7eede4d7f74375c04
queenoko <quimaudongwo@gmail.com>
2024-01-26 15:55:52 +0100

    hpermedia pagination of baby names

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""This is a Pagination helper function.
+"""This the Pagination helper function.
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """This function Retrieves the index range from a given page and page size.
+    """This retrieves the index range from a given page and page size.
     """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
