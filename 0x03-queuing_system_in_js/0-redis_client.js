0564f380752f28b4e97fe22d0f08275cf0d5c575
queenoko <quimaudongwo@gmail.com>
2024-03-05 13:52:35 +0100

    install redis instance

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+// radis instance installation
+import redis from "redis";
+const client = redis.createClient();
+
+client
+  .on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+  })
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  });
+  
