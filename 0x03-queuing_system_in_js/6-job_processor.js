893c1280dbe9b99da139212527c88d0009657c2d
queenoko <quimaudongwo@gmail.com>
2024-03-05 15:20:02 +0100

    redis processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,18 @@
+// kue redis creator
+import kue from "kue";
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+
+const queueKey = "push_notification_code";
+
+queue.process(queueKey, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
